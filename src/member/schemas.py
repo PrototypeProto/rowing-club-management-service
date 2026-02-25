94683e20e8daafeb2d815c8f10b113bbde6c3ac8
prototypeProto <anunfoundx@gmail.com>
2026-02-24 22:25:03 -0500

    updating model schema to reflect current db changes

--- src/member/schemas.py
+++ src/member/schemas.py
@@ -1,31 +1,14 @@
 from pydantic import BaseModel, Field
 from pydantic import model_validator
-import uuid
+from uuid import UUID
 from datetime import date, datetime
 from enum import Enum
+from src.db.db_enum_models import MemberRoleEnum, SemesterEnum
 
 class StrictModel(BaseModel):
     model_config = {
         "extra": "forbid"
     }
-
-class MemberRole(str, Enum):
-    '''
-        May deprecate ADMIN role in favor of president+VP role, TBD
-    '''
-    ADMIN = "admin"
-    COACH = "coach"
-    OFFICER = "officer"
-    MEMBER = "member"
-    INACTIVE = "inactive"
-
-class Semester(str, Enum):
-    '''
-        NO practice during the summer, but placeholder in case situation changes
-    '''
-    SPRING = "spring"
-    SUMMER = "summer"
-    FALL = "fall"
 
 
 
@@ -33,46 +16,47 @@ class Coxwain(StrictModel):
     '''
         Marks members that are coxwains
     '''
-    cox_id: uuid.UUID
+    cox_id: UUID
 
-class CoxwainEvaluation(StrictModel):
-    '''
-        Anonymous feedback on a coxwain's abilities and suggestions from rowers
-    '''
-    cox_id: uuid.UUID
-    semester: str
-    year: int = Field(ge=1900)
+
+class CoxwainEvaluationModel(StrictModel):
+    evaluation_id: int 
+    cox_id: UUID 
+    semester_id: int 
+    date_created: date
     feedback: str
 
-class CoxwainEvaluationResponseModel(StrictModel):
-    evaluation_id: uuid.UUID
-    cox_id: uuid.UUID
-    semester: Semester
-    year: int
-    feedback: str
 
-class CoxwainEvaluationSpecificModel(StrictModel):
-    evaluation_id: uuid.UUID
+class CreateCoxwainEvaluationModel(StrictModel):
+    '''
+        Submit 'anonymous' feedback on a coxwain's abilities and suggestions from rowers
+        user submitted stored in logs not in DB
+    '''
+    cox_id: UUID
+    semester: SemesterEnum
+    feedback: str = Field(nullable=False)
 
-class CoxwainEvaluationSearchModel(StrictModel):
+
+class SearchCoxwainEvaluationModel(StrictModel):
     '''
         Anonymous feedback on a coxwain's abilities and suggestions from rowers
+        Use `semester_id` to actually search
     '''
-    cox_id: uuid.UUID
-    semester: str
+    cox_id: UUID
+    semester: SemesterEnum
     year: int = Field(ge=1900)
 
 class Rower(StrictModel):
     '''
         A member that rows
     '''
-    rower_id: uuid.UUID
+    rower_id: UUID
 
-class RolePermissions(StrictModel):
+class RolePermissionsModel(StrictModel):
     '''
         Restricts certain APIs based on these permissions
     '''
-    role: MemberRole
+    role: MemberRoleEnum
     access_site: bool
     create_announcements: bool
     manage_dates: bool
@@ -81,11 +65,11 @@ class RolePermissions(StrictModel):
     view_funds: bool
     view_roster: bool
 
-class RolePermissionsRequestUpdateModel(StrictModel):
+class RolePermissionsUpdateModel(StrictModel):
     '''
         Flips bool value in database where bool is true
     '''
-    role: MemberRole
+    role: MemberRoleEnum
     access_site: bool = False
     create_announcements: bool = False
     manage_dates: bool = False
@@ -111,38 +95,25 @@ class RolePermissionsRequestUpdateModel(StrictModel):
 
         return self
 
-class RolePermissionsUpdateModel(StrictModel):
-    '''
-        Flips bool value in database where bool is true
-    '''
-    access_site: bool = False
-    create_announcements: bool = False
-    manage_dates: bool = False
-    manage_members: bool = False
-    manage_roles: bool = False
-    view_funds: bool = False
-    view_roster: bool = False
 
-
-class MemberEnrollment(StrictModel):
+class MemberEnrollmentHistoryModel(StrictModel):
     '''
         Tracks membership status on a per-semester basis
     '''
-    member_id: uuid.UUID
-    year: int = Field(ge=1900)
-    semester: str
-    role: str 
-    dues_paid: bool 
+    member_id: UUID
+    semester_id: int 
+    role: MemberRoleEnum 
+    dues_paid: bool
 
-class MemberEnrollmentCreateModel(StrictModel):
+class CreateMemberEnrollmentHistoryModel(StrictModel):
     '''
         Tracks membership status on a per-semester basis
     '''
-    role: MemberRole
-    member_id: uuid.UUID
-    year: int = Field(ge=1900)
-    semester: Semester
-
+    member_id: UUID
+    role: MemberRoleEnum
+    year: int = Field(ge=1900, default=date.year)
+    semester: SemesterEnum
+    dues_paid: bool = False
 
 class UserPrivilegeUpdateModel(StrictModel):
     role: str
