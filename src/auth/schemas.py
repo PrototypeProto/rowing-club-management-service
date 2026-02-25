0cfa8e6ff91bd5d24f288b8225f7bcbdd1e0ebf4
prototypeProto <anunfoundx@gmail.com>
2026-02-24 21:25:10 -0500

    updated schema to be consistent with new db schema

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
-import uuid
+from uuid import UUID
 from datetime import date, datetime
-from sqlmodel import SQLModel
+from src.db.db_enum_models import MemberRoleEnum
 
 '''
     What we use to enforce data input
@@ -12,26 +12,36 @@ class StrictModel(BaseModel):
         "extra": "forbid"
     }
 
-class User(BaseModel):
-    uid: uuid.UUID
-    username: str
-    email: str
+class User(StrictModel):
+    uid: UUID 
+    username: str = Field(min_length=8, max_length=8)
+    email: str = Field(max_length=32)
     passwd_hash: str = Field(exclude=True)
-    role: str
-    is_verified: bool = False
-    date_created: date
-    time_modified: datetime
+    first_name: str = Field(min_length=2)
+    last_name: str = Field(min_length=2)
+    role: MemberRoleEnum
+    birthdate: date
+    is_verified: bool
+    join_date: date
 
-class UserCreateModel(BaseModel):
+class UserCreateModel(StrictModel):
     username: str = Field(max_length=12)
     email: str = Field(max_length=32)
     passwd: str = Field(min_length=6)
+    first_name: str = Field(min_length=2)
+    last_name: str = Field(min_length=2)
+    birthdate: date
+    role: MemberRoleEnum = MemberRoleEnum.UNREGISTERED
 
-class UserUpdateModel(BaseModel):
-    email: str
-    passwd: str
+class UserChangePasswordModel(StrictModel):
+    '''
+    TODO: Perform safety checks on new passwd
+    '''
+    uid: UUID
+    cur_passwd: str
+    new_passwd: str
 
-class UserLoginModel(BaseModel):
-    email: str
-    passwd: str
+class UserLoginModel(StrictModel):
+    username: str = Field(min_length=8, max_length=8)
+    passwd: str = Field(min_length=6)
 
